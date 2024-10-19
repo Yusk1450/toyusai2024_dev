22bcb3ef9d3d9e7ff5ba11dd12a044be904d0df9
Yusk1450 <ishigo@4dpocket.co.jp>
2024-10-20 02:15:01 +0900

    dev: 照明の通信方式の変更

--- obake2024_light/obake2024_light.ino
+++ obake2024_light/obake2024_light.ino
@@ -4,18 +4,16 @@
 #include <ArduinoJson.h>
 
 #include <HTTPClient.h>
+#include <ArduinoOSCWiFi.h>
 
-#define DEVICE_NO 3
 #define LIGHT_PIN 26
 
 String ssid = "hopter_wifi";
 String pwd = "hopter_1450";
 
-const IPAddress ip(192, 168, 0, 202);
+const IPAddress ip(192, 168, 0, 205);
 const IPAddress gateway(192, 168, 0, 1);
 const IPAddress subnet(255, 255, 255, 0);
-
-String url = "http://192.168.0.115:8888/WORKS/NBU/toyusai2024_dev/server/flag";
 
 void setup()
 {
@@ -37,72 +35,22 @@ void setup()
       Serial.println(".");
     }
 
+    OscWiFi.subscribe(55555, "/light_on", onOscLightOn);
+    OscWiFi.subscribe(55555, "/light_off", onOscLightOff);
+
     Serial.println("Initializing complete");
 }
 
 void loop()
 {
-  HTTPClient http;
-
-  http.begin(url);
-  int httpCode = http.GET();
-
-  if(httpCode > 0)
-  {
-    if(httpCode == HTTP_CODE_OK)
-    {
-      String payload = http.getString();
-      Serial.println(payload);
-
-      DynamicJsonDocument doc(1024);
-      DeserializationError error = deserializeJson(doc, payload);
-
-      if (error)
-      {
-        Serial.println("JSON Parse error");
-        return;
-      }
-      if (DEVICE_NO == 1)
-      {
-        bool value = doc[0];
-        digitalWrite(LIGHT_PIN, value ? LOW : HIGH);
-      }
-      else if (DEVICE_NO == 2)
-      {
-        bool value0 = doc[0];
-        bool value1 = doc[1];
-
-        if (value0 && !value1)
-        {
-          digitalWrite(LIGHT_PIN, HIGH);
-          Serial.println("HIGH");
-        }
-        else
-        {
-          digitalWrite(LIGHT_PIN, LOW);
-          Serial.println("LOW");
-        }
-      }
-      else if (DEVICE_NO == 3)
-      {
-        bool value0 = doc[2];
-        bool value1 = doc[3];
-
-        if (value0 && !value1)
-        {
-          digitalWrite(LIGHT_PIN, HIGH);
-          Serial.println("HIGH");
-        }
-        else
-        {
-          digitalWrite(LIGHT_PIN, LOW);
-          Serial.println("LOW");
-        }
-      }
-      
-    }
-  }
-  http.end();
-
-  delay(1000);
+  OscWiFi.update();
 }
+
+void onOscLightOn(const OscMessage& m) {
+  digitalWrite(LIGHT_PIN, HIGH);
+}
+
+void onOscLightOff(const OscMessage& m) {
+  digitalWrite(LIGHT_PIN, LOW);
+}
+
